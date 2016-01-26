812a723f9efe8b74b868ab9ac1ebd5a9092c005d
Jordi Prats <jprats@systemadmin.es>
2016-01-26 16:17:15 +0100

    suport params i arquitectures

---
+++ manifests/params.pp
@@ -0,0 +1,24 @@
+class megaraid::params {
+
+  $basedir_default='/opt/MegaRAID/MegaCli'
+
+  case $::osfamily
+  {
+    'redhat':
+    {
+      case $::operatingsystemrelease
+      {
+        /^[5.7].*/:
+        {
+        }
+        default: { fail('Unsupported RHEL/CentOS version!')  }
+      }
+    }
+    'Debian':
+    {
+      fail('untested')
+    }
+    default: { fail('Unsupported OS!')  }
+  }
+
+}
